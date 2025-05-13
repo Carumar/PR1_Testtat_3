a4efe1a7ec2493f9ba08249cac1fc1da26f82494
Carumar <1720808@stud.hs-mannheim.de>
2025-05-13 12:49:08 +0200

    Aktualisieren von Time.java

--- Time.java
+++ Time.java
@@ -1,13 +1,29 @@
 import java.ult.Scanner;
 public class Time{
 	public static void main(String[] args) {
-	String zeit ="";
-	boolean formatRichtig = true; 
-	Scanner in = new Scanner(System.in);
-	System.out.println("Eingabe Zeit");
-	zeit = in.next();
-	If (zeit.length !=5 || zeit[3] != ":"){
-		formatRichtig =false;
+		String zeit ="";
+		int stunden = 0;
+		int minuten = 0;
+		boolean formatRichtig = true;
+		String[] zeitTeile = new String[2]; 
+		Scanner in = new Scanner(System.in);
+		System.out.println("Eingabe Zeit");
+		zeit = in.next();
+		
+		If (zeit.length !=5 || zeit[3] != ":"){
+			formatRichtig =false;
 		}
+		else{
+			zeitTeile = zeit.split(:);
+			stunden = Integer.parseInt(zeitTeile[0]);
+			minuten = Integer.parseInt(zeitTeile[1]);
+			If (stunden<0 && stunden>=24){
+				formatRichtig =false;
+			}
+			else If (minuten<0 && minuten >=60){
+				formatRichtig =false;
+			}
+		}
+	return formatRichtig;
 	}
 }
