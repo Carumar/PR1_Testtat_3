bce51f33217f11282446148f8cbd555000f5049e
Carumar <1720808@stud.hs-mannheim.de>
2025-05-13 12:28:34 +0200

    Aktualisieren von Time.java

--- Time.java
+++ Time.java
@@ -1,7 +1,13 @@
 import java.ult.Scanner;
 public class Time{
 	public static void main(String[] args) {
-	String Zeit ="";
+	String zeit ="";
+	boolean formatRichtig = true; 
 	Scanner in = new Scanner(System.in);
-}
+	System.out.println("Eingabe Zeit");
+	zeit = in.next();
+	If (zeit.length !=5 || zeit[3] != ":"){
+		formatRichtig =false;
+		}
+	}
 }
