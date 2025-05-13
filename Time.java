0a2190d7e072a76af60b63a02bd09f0da06a1942
wurst <1720808@stud.hs-mannheim.de>
2025-05-13 14:51:28 +0200

    Syntax Fehler behoben

--- Time.java
+++ Time.java
@@ -1,29 +1,31 @@
-import java.ult.Scanner;
-public class Time{
-	public static void main(String[] args) {
-		String zeit ="";
+import java.util.Scanner;
+
+public class Time {
+	public static boolean timeCheck(String[] args) {
+		String zeit = "";
 		int stunden = 0;
 		int minuten = 0;
 		boolean formatRichtig = true;
-		String[] zeitTeile = new String[2]; 
+		String[] zeitTeile = new String[2];
 		Scanner in = new Scanner(System.in);
 		System.out.println("Eingabe Zeit");
 		zeit = in.next();
-		
-		If (zeit.length !=5 || zeit[3] != ":"){
-			formatRichtig =false;
-		}
-		else{
-			zeitTeile = zeit.split(:);
+
+		if (zeit.length() != 5 || zeit.charAt(3) != ':') {
+			formatRichtig = false;
+		} 
+		else {
+			zeitTeile = zeit.split(":");
 			stunden = Integer.parseInt(zeitTeile[0]);
 			minuten = Integer.parseInt(zeitTeile[1]);
-			If (stunden<0 && stunden>=24){
-				formatRichtig =false;
-			}
-			else If (minuten<0 && minuten >=60){
-				formatRichtig =false;
+			if (stunden < 0 && stunden >= 24) {
+				formatRichtig = false;
+			} 
+			else if (minuten < 0 && minuten >= 60) {
+				formatRichtig = false;
 			}
 		}
+		in.close();
 		return formatRichtig;
 	}
 }
